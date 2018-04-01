7a2828049c1d059dddb8d88e6c8a7836b9feae56
kayhankaya <kayhankaya@me.com>
2018-04-02 02:10:55 +0300

    Add fontawesome, Contact detail and footer social media link.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var path = require('path');
 var app = express();
@@ -7,10 +6,10 @@ var bodyParser = require('body-parser');
 var data;
 data = require("./app_server/models/db");
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'./app_server/views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './app_server/views'));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(ejsLayouts);
 app.use('/public', express.static(path.join(__dirname, 'public')));
